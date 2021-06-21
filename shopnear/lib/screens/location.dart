34888673798d996baaa0faa42a85efd92482e77c
adarshskumar <adarshskumar01@gmail.com>
2021-06-21 23:01:11 +0530

    lo

--- shopnear/lib/screens/location.dart
+++ shopnear/lib/screens/location.dart
@@ -7,18 +7,100 @@ class Location extends StatefulWidget {
   _LocationState createState() => _LocationState();
 }
 
+String? dropdownValue;
+List listItem = [
+  "Thiruvanthapuram",
+  "Kollam",
+  "Pathanamthitta",
+  "Alappuzha",
+];
+
 class _LocationState extends State<Location> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
+          SizedBox(
+            height: 25,
+          ),
           Container(
             child: Image.asset(
               'assets/images/loc.PNG',
               fit: BoxFit.contain,
             ),
             width: MediaQuery.of(context).size.width,
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            'Select your location',
+            style: TextStyle(
+              color: Color(0xff181725),
+              fontSize: 26,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Text(
+            'Swithch on your location to stay in tune with \n what’s happening in your area',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xff7C7C7C),
+              fontSize: 16,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 18.0, vertical: 10.0),
+              child: DropdownButton<String>(
+                hint: Text('Select District'),
+                value: dropdownValue,
+                elevation: 16,
+                isExpanded: true,
+                style: const TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 1,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropdownValue = newValue!;
+                  });
+                },
+                items: <String>[
+                  'Thiruvananthapuram',
+                  'Kollam',
+                  'Pathanamthitta',
+                  'Alappuzha',
+                  'Kottyam',
+                  'Idukki',
+                  'Ernakulam',
+                  'Thrissur',
+                  'Palakkad',
+                  'Malappuram',
+                  'Kozhikode',
+                  'Wayanad',
+                  'Kannur',
+                  'Kasargod',
+                ].map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
           ),
         ],
       ),
