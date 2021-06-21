fcd8e717407d73c8f49119d5ce0b7531c4cac857
Karthik Sunil K <karthiksunil038@gmail.com>
2021-06-21 23:41:41 +0530

    splaash screen success

--- shopnear/lib/main.dart
+++ shopnear/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shopnear/screens/loading_screen.dart';
 import 'package:shopnear/screens/location.dart';
 import 'package:shopnear/screens/loginPage.dart';
@@ -14,12 +15,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: Location());
+        home: SplashScreen());
   }
 }
