77d7cf9012332a46220eb75f8b95566438d0ed51
adarshskumar <adarshskumar01@gmail.com>
2021-07-03 22:24:38 +0530

    appbar

--- shopnear/lib/user_screen/home_screen.dart
+++ shopnear/lib/user_screen/home_screen.dart
@@ -9,177 +9,132 @@ import 'package:shopnear/user_screen/items_list/producta_cards.dart';
 class UserHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              height: 100,
-              padding: EdgeInsets.only(
-                left: 130,
-                top: 40,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Icon(
-                    Icons.location_on_sharp,
-                    color: Colors.black,
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Text(
-                    'Kerala, India',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-            ),
-            RoundedInputField(
-              hintText: "Search Store",
-              onChanged: (value) {},
-              icon: Icons.search_rounded,
-            ),
-            CarouselSlider(
-              items: [
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Scaffold(
+        appBar: AppBar(
+          title: RoundedInputField(
+            hintText: "Search Store",
+            onChanged: (value) {},
+            icon: Icons.search_rounded,
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
                 Container(
-                  margin: EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
+                  height: 60,
+                  padding: EdgeInsets.only(
+                    left: 130,
                   ),
-                  child: Image.asset(
-                    "assets/images/b1.jpg",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Image.asset(
-                    "assets/images/b2.jpg",
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Image.asset(
-                    "assets/images/b3.jpg",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Image.asset(
-                    "assets/images/b4.jpg",
-                    fit: BoxFit.contain,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.all(5.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Image.asset(
-                    "assets/images/b5.jpg",
-                    scale: 2,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ],
-              options: CarouselOptions(
-                height: 200,
-                aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                reverse: false,
-                autoPlay: true,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.fastOutSlowIn,
-                enlargeCenterPage: true,
-                onPageChanged: null,
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
-            ListTile(
-              leading: Text(
-                'Find Your Shop',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-              trailing: Text(
-                'See All',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Color(0xff53B175),
-                ),
-              ),
-            ),
-            ItemsShopCards()
-
-            //cards
-
-            /* Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: Row(
-                children: [
-                  Container(
-                    width: 150,
-                    margin: EdgeInsets.only(
-                      top: 10,
-                      bottom: 10,
-                      left: 5,
-                      right: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/"),
-                        fit: BoxFit.cover,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.location_on_sharp,
+                        color: Colors.black,
                       ),
-                      borderRadius: BorderRadius.circular(15),
+                      Text(
+                        'Kerala, India',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ),
+                CarouselSlider(
+                  items: [
+                    Container(
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Image.asset(
+                        "assets/images/b1.jpg",
+                        fit: BoxFit.contain,
+                      ),
                     ),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          top: 5,
-                          left: 5,
-                          child: CircularButton(
-                            iconColor: Colors.blue,
-                            buttonIcon: Icons.add,
-                            buttonAction: () {
-                              print("Create New Story !! ");
-                            },
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 10,
-                          left: 10,
-                          child: Text("rgfrfgb",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                              )),
-                        ),
-                      ],
+                    Container(
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Image.asset(
+                        "assets/images/b2.jpg",
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Image.asset(
+                        "assets/images/b3.jpg",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Image.asset(
+                        "assets/images/b4.jpg",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(5.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Image.asset(
+                        "assets/images/b5.jpg",
+                        scale: 2,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ],
+                  options: CarouselOptions(
+                    height: 200,
+                    aspectRatio: 16 / 9,
+                    viewportFraction: 0.8,
+                    initialPage: 0,
+                    enableInfiniteScroll: true,
+                    reverse: false,
+                    autoPlay: true,
+                    autoPlayInterval: Duration(seconds: 3),
+                    autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    onPageChanged: null,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                ListTile(
+                  leading: Text(
+                    'Find Your Shop',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
                     ),
                   ),
-                ],
-              ),
-            ), */
-          ],
+                  trailing: Text(
+                    'See All',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Color(0xff53B175),
+                    ),
+                  ),
+                ),
+                ItemsShopCards(),
+                ItemsShopCards(),
+                ItemsShopCards(),
+              ],
+            ),
+          ),
         ),
       ),
     );
